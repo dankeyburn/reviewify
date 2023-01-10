cfa60a3060d4e1476b5ccbcef8e901edb031f84e
Dan Keyburn <dan.keyburn@gmail.com>
2023-01-10 13:02:47 -0600

    Add functioning get_artist_albums and get_album_details

--- api/queries/spotify.py
+++ api/queries/spotify.py
@@ -2,20 +2,23 @@ import requests
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-# from secrets import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
-
-
-SPOTIPY_CLIENT_ID='ff1b739424b04c5caaf1bdbd554266b0'
-SPOTIPY_CLIENT_SECRET='e9b9556ce74d4abf963c8ef016b45c72'
-SPOTIPY_REDIRECT_URI='https://musicreviewify.com/callback/'
+from secrets import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 
 
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 class SpotifyQueries:
-    def get_artist(self, name: str):
+    def get_artist_albums(self, name: str):
         results = sp.search(name, type="artist")
         artist_id = results['artists']['items'][0]['id']
         artist_albums = sp.artist_albums(artist_id)
-        return artist_albums
+        albums = artist_albums['items']
+        for a in albums:                                # removing the "available_markets" list
+            del a["available_markets"]                  # to make it easier to read the returned object
+        return artist_albums                            # from the albums object
+
+    def get_album_details(self, album_id: str):
+        album_details = sp.album(album_id)              # removing the "available_markets" list
+        del album_details["available_markets"]          # to make it easier to read the returned object
+        return album_details                            # from the albums object
--- api/routers/spotify.py
+++ api/routers/spotify.py
@@ -4,5 +4,9 @@ from queries.spotify import SpotifyQueries
 router = APIRouter()
 
 @router.get('/api/artists/{name}')
-def get_artist(name: str, repo: SpotifyQueries = Depends()):
-    return repo.get_artist(name)
+def get_artist_albums(name: str, repo: SpotifyQueries = Depends()):
+    return repo.get_artist_albums(name)
+
+@router.get('/api/albums/{album_id}')
+def get_album_details(album_id: str, repo: SpotifyQueries = Depends()):
+    return repo.get_album_details(album_id)
