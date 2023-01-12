b2284973be9d0a97f6308edf2081fca817480ae6
Ethan Beber <efbeber@gmail.com>
2023-01-12 13:22:30 -0500

    adding search bar

---
+++ ghi/src/Search.js
@@ -0,0 +1,98 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import {SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET} from './api/secrets';
+
+const CLIENT_ID = SPOTIPY_CLIENT_ID;
+const CLIENT_SECRET = SPOTIPY_CLIENT_SECRET ;
+
+
+
+function SearchBar() {
+  const [ searchInput, setSearchInput] =useState('');
+//   const [accessToken, setAccessToken] =useState('');
+  const [albums, setAlbums] = useState([]);
+
+// useEffect(() => {
+// API Access Token
+// var authParameters = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded'
+//   },
+//   body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+// }
+
+// fetch('https://accounts.spotify.com/api/token', authParameters )
+//   .then(result => result.json())
+//   .then(data => setAccessToken(data.access_token))
+// }, [])
+
+// Search
+async function search() {
+  console.log("search for " + searchInput);
+
+  // Get request using search to get the artist ID
+  var searchParameters = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken
+    }
+  }
+  var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist' , searchParameters )
+    .then(response => response.json())
+    .then(data => { return data.artists.items[0].id})
+
+  console.log('Artist ID is ' + artistID)
+  // Get request with artist ID grab all the albums from that artist
+  var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setAlbums(data.items);
+    });
+  // Display those albums to the user
+}
+console.log(albums)
+
+  return (
+    <div className="App">
+      <Container>
+        <InputGroup className='mb-3' size='lg'>
+          <FormControl
+          placeholder='Search For Artist'
+          type='input'
+          onKeyDown={event => {
+            if (event.key == 'Enter') {
+              search();
+            }
+          }}
+          onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className='mx-2 row row-cols-4'>
+          {albums.map( ( album, i) => {
+            console.log(album);
+            return (
+        <Card>
+          <Card.Img src={album.images[0].url} />
+          <Card.Body>
+            <Card.Title>{album.name}</Card.Title>
+          </Card.Body>
+        </Card>
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default SearchBar;
