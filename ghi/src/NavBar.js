020a0a8ff7f4b16607ae8402bc3c344d877f85d7
Noah Maurer <noahmaurer12@gmail.com>
2023-01-11 18:21:11 -0600

    joe and noah's best idea yet

--- ghi/src/NavBar.js
+++ ghi/src/NavBar.js
@@ -3,25 +3,26 @@ function NavBar() {
         <nav
             className="navbar navbar-expand-md navbar-dark"
             style={{
-                backgroundColor: "#E7772F",
+                backgroundColor: "#efeee8",
                 fontFamily: "Gill Sans, sans-serif",
                 height: "120px",
                 borderBottom: "2px solid black",
             }}>
             <div className="container-fluid">
                 <img
-                    src="https://kornan.dreamhosters.com/musicr/"
+                    src={require("./output-onlinepngtools.png")}
                     alt="Music Reviewify Logo"
-                    height={90}
+                    height={110}
                 />
                 <div>
                     <button
                         className="btn btn-primary"
                         style={{
                             marginRight: "10px",
-                            backgroundColor: "#3E6D9C",
-                            color: "white",
-                            borderColor: "#3E6D9C",
+                            backgroundColor: "#c69f3a",
+                            color: "#efeee8",
+                            borderColor: "#c69f3a",
+                            border: "2px, solid, black",
                         }}>
                         Login
                     </button>
@@ -29,9 +30,10 @@ function NavBar() {
                         className="btn btn-primary"
                         style={{
                             marginRight: "10px",
-                            backgroundColor: "#001253",
-                            color: "white",
-                            borderColor: "#001253",
+                            backgroundColor: "#c69f3a",
+                            color: "#efeee8",
+                            borderColor: "#c69f3a",
+                            border: "2px solid black",
                         }}>
                         Signup
                     </button>
