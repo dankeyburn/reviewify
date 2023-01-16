619f3187c3337423e0bd9e0bf8ffaa84794ccfda
Noah Maurer <noahmaurer12@gmail.com>
2023-01-16 15:17:28 -0600

    modal styling

--- ghi/src/AlbumModal.js
+++ ghi/src/AlbumModal.js
@@ -42,10 +42,11 @@ function AlbumModal(props) {
             />
 
             <Modal
+                size="lg"
                 key={props.album_id}
                 show={show}
                 onHide={() => setShow(false)}
-                dialogClassName="modal-90w"
+                className="modal fade bd-example-modal-lg"
                 aria-labelledby="example-custom-modal-styling-title">
                 <Modal.Header closeButton>
                     <Modal.Title id="example-custom-modal-styling-title">
@@ -53,34 +54,74 @@ function AlbumModal(props) {
                     </Modal.Title>
                     {/* <Modal.Img src={album.images?[0].url} /> */}
                 </Modal.Header>
-                <Modal.Body>
-                    <div>Album Title: {album.name}</div>
-                    <div>Label: {album.label}</div>
-                    <div>Release Date: {album.release_date}</div>
-                    <div>{props.album_id}</div>
-                    <div>
+                <Modal.Body
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(5, 1fr)",
+                        gridTemplateRows: "repeat(5, 1fr)",
+                        gridColumnGap: "0px",
+                        gridRowGap: "0px",
+                    }}>
+                    <div
+                        style={{
+                            gridArea: "1 / 3 / 3 / 6",
+                            marginLeft: "10px",
+                            marginRight: "10px",
+                        }}>
+                        <div
+                            style={{
+                                fontWeight: "bold",
+                                textAlign: "center",
+                                fontSize: "30px ",
+                                marginBottom: "10px",
+                                borderBottom: "2px solid black",
+                            }}>
+                            {album.name}
+                        </div>
+
+                        <div>Release Date: {album.release_date}</div>
+                    </div>
+
+                    <div
+                        style={{
+                            // height: "100px",
+                            gridArea: "1 / 1 / 3 / 3",
+                        }}>
                         <img
                             src={props.img_url}
                             alt=""
-                            style={{ height: "300px" }}
+                            style={{ objectFit: "contain", maxWidth: "100%" }}
                         />
                     </div>
+                    <div
+                        style={{
+                            gridArea: "3 / 4 / 6 / 6",
+                            height: "30px",
+                            marginTop: "30px",
+                        }}>
+                        <ol>
+                            Tracks
+                            {album.tracks?.items.map((track) => {
+                                return <li key={track.id}>{track.name}</li>;
+                            })}
+                        </ol>
+                    </div>
 
-                    <ol>
-                        Tracks
-                        {album.tracks?.items.map((track) => {
-                            return <li key={track.id}>{track.name}</li>;
-                        })}
-                    </ol>
                     {reviews !== [] ? (
-                        <div>
+                        <div
+                            style={{
+                                gridArea: "3 / 1 / 6 / 4",
+                                marginTop: "30px",
+                            }}>
                             <div>
                                 Reviews:
                                 {reviews.map((review) => {
                                     return (
                                         <div
                                             key={review.id}
-                                            style={{ marginBottom: "20px" }}>
+                                            style={{
+                                                marginBottom: "20px",
+                                            }}>
                                             <div>Rating: {review.rating}</div>
                                             <div>Content: {review.content}</div>
                                             <div>
