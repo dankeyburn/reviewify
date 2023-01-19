54801f7bb5e769976513d4d805b8c68367df3da4
Noah Maurer <noahmaurer12@gmail.com>
2023-01-19 17:28:59 -0600

    review modal clickable

--- ghi/src/SeeReviews.js
+++ ghi/src/SeeReviews.js
@@ -25,60 +25,32 @@ function SeeReviews(props) {
             );
     }, []);
 
-
-
     return (
         <>
-         <div className="App" style={{ marginTop: "30px"}}></div>
+            <div className="App" style={{ marginTop: "30px" }}></div>
             {reviews !== [] ? (
-            <container>
-                <div className="row justify-content-center">
+                <container>
+                    <div className="row justify-content-center">
                         {reviews.map((review) => {
                             return (
-                                <div key={review.id}
+                                <div
+                                    key={review.id}
                                     className="card mb-3 w-100 justify-content-around"
-                                    style={{ maxWidth: "540px"}}
-                                    ><ReviewModal
-                                    id={review.img_url}
+                                    style={{ maxWidth: "540px" }}>
+                                    <ReviewModal
+                                        id={review.id}
+                                        img_url={review.img_url}
+                                        title={review.title}
+                                        rating={review.rating}
+                                        best_song={review.best_song}
+                                        worst_song={review.worst_song}
+                                        reviewer_id={review.reviewer_id}
                                     />
-                                    <div className="row no-gutters">
-                                        <div className="col-md-4" style={{display: 'flex', alignItems: 'center'}}>
-                                            <img
-                                                src={review.img_url}
-                                                className="card-img"
-                                                alt="..."
-                                                style={{paddingLeft: '15px'}}
-                                            />
-                                        </div>
-                                        <div className="col-md-8">
-                                            <div className="card-body">
-                                                <h5 className="card-title">
-                                                    {review.title}
-                                                </h5>
-                                                <p className="card-text">
-                                                    Rating: {review.rating} out of 5
-                                                </p>
-                                                <p className="card-text">
-                                                    Best Song: {review.best_song}
-                                                </p>
-                                                <p className="card-text">
-                                                    Worst Song: {review.worst_song}
-                                                </p>
-                                                <p className="card-text">
-                                                    <small className="text-muted">
-                                                        Reviewer:{" "}
-                                                        {review.reviewer_id}
-                                                    </small>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             );
                         })}
-                        </div>
-            </container>
-
+                    </div>
+                </container>
             ) : (
                 <div>No Reviews Yet</div>
             )}
@@ -88,20 +60,19 @@ function SeeReviews(props) {
 
 export default SeeReviews;
 
-
- // <div>
-                //     <div>
-                //         Reviews:
-                //         {reviews.map((review) => {
-                //             return (
-                //                 <div key={review.id}>
-                //                     <ReviewModal
-                //                     id={review.id}
-                //                     title={review.title}/>
-                //                     <div>Rating: {review.rating}</div>
-                //                     <div>Reviewer ID: {review.reviewer_id}</div>
-                //                 </div>
-                //             );
-                //         })}
-                //     </div>
-                // </div>
+// <div>
+//     <div>
+//         Reviews:
+//         {reviews.map((review) => {
+//             return (
+//                 <div key={review.id}>
+//                     <ReviewModal
+//                     id={review.id}
+//                     title={review.title}/>
+//                     <div>Rating: {review.rating}</div>
+//                     <div>Reviewer ID: {review.reviewer_id}</div>
+//                 </div>
+//             );
+//         })}
+//     </div>
+// </div>
