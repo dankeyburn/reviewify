6d91c9c536f022a7ea90cafb298d1751c0353209
Dan Keyburn <dan.keyburn@gmail.com>
2023-01-24 12:40:21 -0600

    First draft of tackling the READMEs

--- ghi/README.md
+++ ghi/README.md
@@ -1,70 +1,35 @@
-# Getting Started with Create React App
+# Music Reviewify
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- Ethan Beber
+- Dan Keyburn
+- Joe Lock
+- Noah Maurer
+- Kornebari Nantah
 
-## Available Scripts
+## Design
 
-In the project directory, you can run:
+- [API Design](docs/apis.md)
+- [Data model](docs/data-model.md)
+- [GHI](docs/ghi.md)
+- [API Design](docs/apis.md)
 
-### `npm start`
+## Intended market
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Lorem ipsum dolor sit amet, consectetur adip
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Functionality
 
-### `npm test`
+- Visitors can do this
+- Logged-in users can do that
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Project Initilization
 
-### `npm run build`
+To fully enjoy this application on your local machine, please make sure to follow these steps:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+1. Clone the repository down to your local machine
+2. CD into the new project directory
+3. Run `docker volume create pg-admin`
+4. Run `docker volume create music`
+5. Run `docker compose build`
+6. Run `docker compose up`
+7. Exit the container's CLI, and enjoy Music Reviewify's music review database.
--- ghi/src/Search.js
+++ ghi/src/Search.js
@@ -1,10 +1,10 @@
 import {
-    Container,
-    InputGroup,
-    FormControl,
-    Button,
-    Row,
-    Card,
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
 } from "react-bootstrap";
 import { useState } from "react";
 import AlbumModal from "./AlbumModal";
@@ -13,73 +13,74 @@ import React, { useContext } from "react";
 import { Context } from "./Store";
 
 export default function SearchBar() {
-    const [searchInput, setSearchInput] = useState("");
-    const [albums, setAlbums] = useState([]);
-    const [text, setText] = useState("");
-    const [state, dispatch] = useContext(Context);
+  const [searchInput, setSearchInput] = useState("");
+  const [albums, setAlbums] = useState([]);
+  const [text, setText] = useState("");
+  const [state, dispatch] = useContext(Context);
 
-    function search() {
-        fetch(`${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/artists/${searchInput}`)
-            .then((response) => response.json())
-            .then((data) => {
-                let albums_with_dupes = data.items;
-                const seen = new Set();
-                const uniqueAlbums = albums_with_dupes.filter((album) => {
-                    const duplicate = seen.has(album.name);
-                    seen.add(album.name);
-                    return !duplicate;
-                });
-                setAlbums(uniqueAlbums);
-            });
-    }
-    function Display() {
-        document.getElementById("hide-item1").style.display = "none";
-        document.getElementById("hide-item2").style.display = "none";
-    }
+  function search() {
+    fetch(
+      `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/artists/${searchInput}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let albums_with_dupes = data.items;
+        const seen = new Set();
+        const uniqueAlbums = albums_with_dupes.filter((album) => {
+          const duplicate = seen.has(album.name);
+          seen.add(album.name);
+          return !duplicate;
+        });
+        setAlbums(uniqueAlbums);
+      });
+  }
+  function Display() {
+    document.getElementById("hide-item1").style.display = "none";
+    document.getElementById("hide-item2").style.display = "none";
+  }
 
-    return (
-        <div className="App" style={{ marginTop: "30px" }}>
-            <Container>
-                <InputGroup className="mb-3" size="lg">
-                    <FormControl
-                        placeholder="Search For Artist"
-                        type="input"
-                        onKeyDown={(event) => {
-                            if (event.key === "Enter") {
-                                search();
-                                Display();
-                            }
-                        }}
-                        onChange={(event) => setSearchInput(event.target.value)}
-                    />
-                    <Button
-                        onClick={search}
-                        style={{ border: "2px solid #efeee8" }}>
-                        Search
-                    </Button>
-                </InputGroup>
-            </Container>
-            <Container>
-                <Row className="mx-2 row row-cols-4">
-                    {albums.map((album, i) => {
-                        return (
-                            <Card
-                                key={album.id}
-                                variant="primary"
-                                style={{
-                                    padding: "0px",
-                                    marginBottom: "20px",
-                                }}>
-                                <AlbumModal
-                                    album_id={album.id}
-                                    img_url={album.images[0].url}
-                                    album_name={album.name}
-                                />
-                            </Card>
-                        );
-                    })}
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App" style={{ marginTop: "30px" }}>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+                Display();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search} style={{ border: "2px solid #efeee8" }}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map((album, i) => {
+            return (
+              <Card
+                key={album.id}
+                variant="primary"
+                style={{
+                  padding: "0px",
+                  marginBottom: "20px",
+                }}
+              >
+                <AlbumModal
+                  album_id={album.id}
+                  img_url={album.images[0].url}
+                  album_name={album.name}
+                />
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 }
