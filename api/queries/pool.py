fd90aef65a510655db7a01428c3addfa5dceab49
Noah Maurer <noahmaurer12@gmail.com>
2023-01-31 10:45:50 -0600

    pool edited

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -2,6 +2,4 @@ import os
 from psycopg_pool import ConnectionPool
 
 
-# pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
-# pool = ConnectionPool(conninfo=f"process.env{DATABASE_URL}")
-pool = ConnectionPool(conninfo=process.env["DATABASE_URL"])
+pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
