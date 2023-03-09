adcb74e4be13f91e67e726cd995971650a9fdcf1
Dan Keyburn <dan.keyburn@gmail.com>
2023-03-09 14:07:36 -0600

    loading initial commit

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers import accounts, reviews, spotify
@@ -8,7 +9,9 @@ import os
 app = FastAPI()
 
 origins = [
+    "http://localhost",
     "http://localhost:3000",
+    "http://localhost:8000",
     "https://team-drop-and-give-me-20.gitlab.io/module3-project-gamma/",
     "https://reviewify.sept-ct-2.mod3projects.com",
     os.environ.get("REACT_APP_SAMPLE_SERVICE_API_HOST", None),
--- ghi/src/NavBar.js
+++ ghi/src/NavBar.js
@@ -8,68 +8,58 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function NavBar() {
-    const [state] = useContext(Context);
+  const [state] = useContext(Context);
 
-    return (
-        <nav className="navbar navbar-expand-md navbar-dark">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">
-                    <img
-                        id="light-nav-logo"
-                        src={require("./images/output-onlinepngtools.png")}
-                        alt="Music Reviewify Logo"
-                        height={110}
-                    />
-                    <img
-                        id="dark-nav-logo"
-                        src={require("./images/dark-reviewify-png.png")}
-                        alt="Music Reviewify Logo"
-                        height={110}
-                    />
-                </NavLink>
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          <img
+            id="light-nav-logo"
+            src={require("./images/output-onlinepngtools.png")}
+            alt="Music Reviewify Logo"
+            height={110}
+          />
+          <img
+            id="dark-nav-logo"
+            src={require("./images/dark-reviewify-png.png")}
+            alt="Music Reviewify Logo"
+            height={110}
+          />
+        </NavLink>
 
-                <div>
-                    {state.currentAccount["id"] ? (
-                        <div className="hello-message">
-                            Hello, {state.currentAccount["username"]}!
-                        </div>
-                    ) : (
-                        <></>
-                    )}
-                </div>
-                <div>
-                    {state.token ? (
-                        <>
-                            <Link
-                                className="navbar-brand all-reviews"
-                                to="/reviews/all">
-                                <Button className="btn btn-primary">
-                                    All Reviews
-                                </Button>
-                            </Link>
-                            <Logout />
-                            <NavLink
-                                className="navbar-brand"
-                                to="/reviews/user">
-                                <Button>My Reviews</Button>
-                            </NavLink>
-                        </>
-                    ) : (
-                        <>
-                            <Link
-                                className="navbar-brand all-reviews"
-                                to="/reviews/all">
-                                <Button className="btn btn-primary">
-                                    All Reviews
-                                </Button>
-                            </Link>
-                            <LoginModal />
-                            <SignupModal />
-                        </>
-                    )}
-                </div>
+        <div>
+          {state.currentAccount["id"] ? (
+            <div className="hello-message">
+              Hello, {state.currentAccount["username"]}!
             </div>
-        </nav>
-    );
+          ) : (
+            <></>
+          )}
+        </div>
+        <div>
+          {state.token ? (
+            <>
+              <Link className="navbar-brand all-reviews" to="/reviews/all">
+                <Button className="btn btn-primary">All Reviews</Button>
+              </Link>
+              <Logout />
+              <NavLink className="navbar-brand" to="/reviews/user">
+                <Button className="btn btn-primary">My Reviews</Button>
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <Link className="navbar-brand all-reviews" to="/reviews/all">
+                <Button className="btn btn-primary">All Reviews</Button>
+              </Link>
+              <LoginModal />
+              <SignupModal />
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default NavBar;
